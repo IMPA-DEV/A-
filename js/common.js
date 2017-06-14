1d5143c567d97bc06e179e26d3adc34da2032b33
MAXON-BY <max_on.89@mail.ru>
2017-06-14 10:40:13 +0300

    Добавлены все блоки, футер  кроме калькулятора. НЕ подключены слайдеры и попАп

--- js/common.js
+++ js/common.js
@@ -1,12 +1,5 @@
 $(function() {
 
-	//SVG Fallback
-	if(!Modernizr.svg) {
-		$("img[src*='svg']").attr("src", function() {
-			return $(this).attr("src").replace(".svg", ".png");
-		});
-	};
-
 	//E-mail Ajax Send
 	//Documentation & Example: https://github.com/agragregra/uniMail
 	$("form").submit(function() { //Change
@@ -25,23 +18,8 @@ $(function() {
 		return false;
 	});
 
-	//Chrome Smooth Scroll
-	try {
-		$.browserSelector();
-		if($("html").hasClass("chrome")) {
-			$.smoothScroll();
-		}
-	} catch(err) {
+	/* twenty IMG */
 
-	};
-
-	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
-
-	
-});
-
-$(window).load(function() {
-	$(".loader_inner").fadeOut();
-	$(".loader").delay(400).fadeOut("slow");
 
 });
+
